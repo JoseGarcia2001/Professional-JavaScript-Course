b6767a029c61fcb00db3fd6a48a1a0518762849e
Jose Garcia <jagarcia7655@gmail.com>
2020-07-27 14:49:29 -0500

    Maquetando el media player y agregando estilos

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-import mediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/AutoPlay.js";
-
-const video = document.querySelector("video");
-const button = document.querySelector("button");
-
-const player = new mediaPlayer({ value: video, plugins: [new AutoPlay()] });
-button.onclick = () => player.playOrPause();
