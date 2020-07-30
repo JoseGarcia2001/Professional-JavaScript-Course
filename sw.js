a60188f587e5494550af6e81f60c89597030f3d8
Jose Garcia <jagarcia7655@gmail.com>
2020-07-29 19:35:36 -0500

    Terminando Service Workers y APIS del DOM

--- sw.js
+++ sw.js
@@ -2,6 +2,18 @@ self.addEventListener("install", (event) => {
   event.waitUntil(precache());
 });
 
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(cachedResponse(request));
+
+  event.waitUntil(updateCache(request));
+});
+
 async function precache() {
   const cache = await caches.open("v1");
   return cache.addAll([
@@ -20,18 +32,14 @@ async function precache() {
   ]);
 }
 
-self.addEventListener("fetch", (event) => {
-  const request = event.request;
-
-  if (request.method !== "GET") {
-    return;
-  }
-
-  event.respondWith(cachedResponse(request));
-});
-
 async function cachedResponse(request) {
   const cache = await caches.open("v1");
   const response = await cache.match(request);
   return response || fetch(request);
+}
+
+async function updateCache(request) {
+  const cache = await caches.open("v1");
+  const response = await fetch(request);
+  return cache.put(request, response);
 }
