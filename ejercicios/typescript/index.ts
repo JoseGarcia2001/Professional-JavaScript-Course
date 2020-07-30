e3f02ec8dffd631cded2b35045dcb720aee53e81
Jose Garcia <jagarcia7655@gmail.com>
2020-07-29 21:58:42 -0500

    Aprendiendo y configurando TypeScript

---
+++ ejercicios/typescript/index.ts
@@ -0,0 +1,16 @@
+//Variables por tipo
+// String
+let person: string = "Person";
+// number
+let age: number = 32;
+// boolean
+let muted: boolean = true;
+
+// ARRAYS
+let people: string[] = [];
+people = ["Martha", "Hugo"];
+// ARRAYS VARIADOS
+let peopleAndAges: Array<string | number> = [];
+peopleAndAges = ["Pedro", 13];
+
+// Enums como arrays con un valor en específico
