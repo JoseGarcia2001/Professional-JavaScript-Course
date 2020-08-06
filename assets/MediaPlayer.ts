8327738babe5f563d65a73391c89522c09506a3a
Jose Garcia <jagarcia7655@gmail.com>
2020-08-06 00:13:18 -0500

    Terminando los plugins de Ads

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -2,10 +2,20 @@ class MediaPlayer {
   media: HTMLMediaElement;
   plugins: Array<any>;
   pausedByUser: boolean;
+  container: HTMLElement;
+
   constructor(config) {
     (this.media = config.value), (this.plugins = config.plugins || []);
     this.pausedByUser = false;
+    this.initPlayer();
     this.autoRun();
+  }
+
+  initPlayer() {
+    this.container = document.createElement("div");
+    this.container.style.position = "relative";
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media);
   }
 
   private autoRun() {
